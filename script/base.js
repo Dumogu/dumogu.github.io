5c3956cff88c22106cc40a0829c52f19813be970
Panda <646318717@qq.com>
2023-11-20 07:45:34 +0000

    增加身份证转换模板

--- script/base.js
+++ script/base.js
@@ -1,7 +1,13 @@
-function myFunction() {
-   document.getElementById("demo").innerHTML = "这是一个测试，表示程序已经运行。";
-}
 
-function write(msg){
-   document.write(msg)
+function myFunction(num) {
+   // 获取 id = "numb" 的输入字段的值
+   let x = document.getElementById("idcard").value;
+   // 如果 x 不是数字或小于 1 或大于 10
+   let text;
+   if (isNaN(x) || x < 1 || x > 10) {
+      text = "输入无效";
+   } else {
+      text = "输入没问题";
+   }
+   document.getElementById("newidcard").innerHTML = text;
 }
