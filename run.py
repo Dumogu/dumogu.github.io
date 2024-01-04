e2f66e304ecd0897fd91b49d6fdd3d8ceeacac9b
熊亚飞 <646318717@qq.com>
2024-01-04 09:43:12 +0800

    网页更新@river09:42

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/beta/')
+def beta():
     return render_template('beta.html')
 
 if __name__ == '__main__':
